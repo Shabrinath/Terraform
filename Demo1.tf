1cd61968a62874796f1b4a6e56c9ffdc7b5c57f7
Shabrinath <38928260+Shabrinath@users.noreply.github.com>
2021-12-16 09:56:34 -0600

    Demo1.tf
    Terraform with conditional expression

---
+++ Demo1.tf
@@ -0,0 +1,26 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "3.68.0"
+    }
+  }
+}
+
+provider "aws" {
+    region = "us-west-2"
+    access_key = "XXXXXXXXXX"
+    secret_key = "XXXXXXXXXXXXXXXXXXXX"
+}
+
+resource "aws_instance" "dev" {
+   ami = "ami-082b5a644766e0e6f"
+   instance_type = "t2.micro"
+   count = var.istest == true ? 1 : 0
+}
+
+resource "aws_instance" "prod" {
+   ami = "ami-082b5a644766e0e6f"
+   instance_type = "t2.large"
+   count = var.istest == false ? 1 : 0
+}
