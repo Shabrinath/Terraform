30dcc7953a4661eb4c514c630fb049eb934d450b
Shabrinath <38928260+Shabrinath@users.noreply.github.com>
2022-02-01 20:27:17 -0600

    Update az-resourcegroup.tf

--- az-resourcegroup.tf
+++ az-resourcegroup.tf
@@ -7,10 +7,6 @@ provider "azurerm" {
 }
 
 
-#resource "random_pet" "rg-name" {
-#  prefix    = var.resource_group_name_prefix
-#}
-
 resource "azurerm_resource_group" "terra-rg" {
   name      = "Terraform-RG"
   location  = "West Europe"
